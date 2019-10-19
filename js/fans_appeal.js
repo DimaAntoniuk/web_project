ac254c81ec60fa3c205a1cad0ba184d37a7b9f28
DimaAntoniuk <dimaantoniuk09@gmail.com>
2019-10-19 19:29:22 +0300

    added fans_appeal.js

--- js/fans_appeal.js
+++ js/fans_appeal.js
@@ -1,3 +1,22 @@
-$('.send-button').click(function() {
-  console.log("1x");
-})
+document.addEventListener('DOMContentLoaded', function(){
+  document.getElementById('send').addEventListener('click', function() {
+    var appeal_text = document.getElementById('text').value;
+    if(appeal_text !== '') {
+      var element = document.createElement('li');
+      var hr = document.createElement('hr');
+      var big_div = document.createElement('div');
+      var left_div = document.createElement('div');
+      var right_div = document.createElement('div');
+      big_div.setAttribute('class', 'block d-flex');
+      left_div.setAttribute('class', 'both left text-center');
+      right_div.setAttribute('class', 'both right');
+      left_div.innerHTML = 'Dima Antinuk <br> 22:18 <br> 08.10.2019';
+      right_div.innerHTML = appeal_text;
+      big_div.appendChild(left_div);
+      big_div.appendChild(right_div);
+      element.appendChild(hr);
+      element.appendChild(big_div);
+      document.getElementById('appeals').appendChild(element);
+    }
+  });
+});
